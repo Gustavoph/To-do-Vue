46059cd9183b128565375cdb3eb506d02630396c
Gustavoph <gusta.coliveira07@gmail.com>
2021-03-13 00:43:45 -0300

    Initial commit

---
+++ src/components/NewTask.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="new-task">
+        <input v-model="name" @keydown.enter="add" type="text"
+        class="form-element" placeholder="Nova Tarefa?">
+        <button class="form-element" @click="add">+</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: ''
+        }
+    },
+    methods:{
+        add(){
+            this.$emit('taskAdded', { name: this.name, pending: true})
+            this.name = ''
+        }
+    }
+}
+</script>
+
+<style>
+    .new-task {
+        margin: 35px;
+    }
+
+    .form-element{
+        outline: none;
+        font-size: 2rem;
+        border: 1px solid #fff;
+        padding: 5px 10px 8px;
+        color: white;
+    }
+
+    input.form-element {
+        width: 500px;
+        background-color: #fff2;
+        border-radius: 8px 0px 0px 8px;
+    }
+
+    button.form-element {
+        border-left: none;
+        background: #2196f3;
+        border-radius: 0px 8px 8px 0px;
+    }
+
+</style>
---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="task-grid">
+        <template v-if="tasks.length > 0">
+            <Task v-for="(task, i) in tasks" 
+            @taskDeleted="$emit('taskDeleted', i)"
+            @taskCompleted="$emit('taskCompleted', i)"
+            :key="task.name" 
+            :task="task"></Task>
+        </template>
+        <template v-else>
+            <div class="no-task">
+                <h2>Nenhuma Tarefa cadastrada</h2>
+            </div>
+        </template>
+    </div>
+</template>
+
+<script>
+import Task from './Task.vue';
+export default {
+    components: { Task },
+    props: {
+        tasks:{
+            type: Array,
+            required: true
+        }
+    },
+    
+}
+</script>
+
+<style scoped>
+    .task-grid {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+    .task-grid .task{
+        margin: 10px;
+    }
+    .no-task{
+        color: #aaa;
+        font-size: 1.7rem;
+    }
+</style>
